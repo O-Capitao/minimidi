76f9deee2669438618f025839be957319ee12c75
Francisco Correia <francisco.caetano.correia@gmail.com>
2025-04-30 08:25:46 +0100

    add cols rendering

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
-#include "stdio.h"
-#include "string.h"
-#include "stdlib.h"
-#include "stdbool.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+
 #include "minimidi.h"
 #include "globals.h"
+
 
 // TUI
 #include <ncurses.h>
@@ -17,22 +21,30 @@ static const char *ALL_NOTES[] = {"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F"
 static const int OCT_RANGE = 8;
 static const int MAX_NOTE_VAL = OCT_RANGE * 12;
 
-static const int LINES_PER_NOTE = 2;
-static const int COLS_PER_BEAT = 1;
+static const int LINES_PER_SEMITONE = 2;
+static const int COLS_PER_BEAT = 2;
 
-// size things
-static const int VE_LEFT_LABELS_WIDTH = 5;
-static const int VE_BOTT_LABELS_HEIGHT = 3;
+// Grid Subcomponent
+static const int GRID_LEFT_LABELS_WIDTH = 8;
+static const int GRID_BOTT_LABELS_HEIGHT = 3;
+static const int GRID_PADDING_TOP = 2;
+
 
 static const int TOP_BAR_HEIGHT = 1;
 static const int TOP_RIGHT_WIDTH = 10;
 static const int BOTT_BAR_HEIGHT = 1;
 
+// aux for debugging
+static int WIN_SIZE[2];
+
+/***
+ * AUX STRUCTS!
+ */
 typedef struct UiState
 {
     // ZOOM
-    int bars_in_screen;
-    int notes_in_screen;
+    int beats_in_grid;
+    int semitones_in_grid;
     
     int logical_start[2]; // cols, lines
     int key_press_displacement[2]; // how much do we move each arrow key press?
@@ -47,8 +59,8 @@ void UiState_init( UiState *self )
     self->is_dirty = false;
     self->is_running = true;
     //
-    self->bars_in_screen = 4;
-    self->notes_in_screen = 12;
+    self->beats_in_grid = 0;
+    self->semitones_in_grid = 0;
     //
     self->logical_start[0] = 0;
     self->logical_start[1] = 0;
@@ -58,57 +70,57 @@ void UiState_init( UiState *self )
 
     self->grid_size[0] = 0;
     self->grid_size[1] = 0;
-
+    
 }
-
-void make_line( char *line_str,  int size )
-{
-    for (int i = 0; i < size -1 ; i++)
-    {
-        line_str[i] = '_';
-    }
-
-    line_str[size - 1] = '\0';
-}
-
 void UiState_update_sizes( UiState *self, int sz_cols, int sz_lines )
 {
     // how many notes can we see on the screen?
-    self->grid_size[0] = sz_cols - VE_LEFT_LABELS_WIDTH - 2;
-    self->grid_size[1] = sz_lines - VE_BOTT_LABELS_HEIGHT - 2;
+    self->grid_size[0] = sz_cols - GRID_LEFT_LABELS_WIDTH - 2;
+    self->grid_size[1] = sz_lines - GRID_BOTT_LABELS_HEIGHT - 2;
 
-    self->notes_in_screen = sz_lines / LINES_PER_NOTE;
-    self->bars_in_screen = sz_cols / ( 4 * COLS_PER_BEAT);
+    self->semitones_in_grid = self->grid_size[1] / LINES_PER_SEMITONE;
+    self->beats_in_grid = self->grid_size[0] / ( COLS_PER_BEAT);
 
-    printf("s");
 }
 
+/***
+ * RENDER!
+ */
 int render_grid( WINDOW *tgt, UiState *state)
 {
-    int line_index;
-    int line_size = state->grid_size[0] - VE_LEFT_LABELS_WIDTH;
-    char line[ line_size ];
-    make_line(line, line_size );
+    int line_index, col_index;
+    
+    chtype note_delim = '_';
+    chtype bar_delim = ':';
 
-    for (int i_note = state->logical_start[1]; i_note < state->logical_start[1] + state->notes_in_screen; i_note ++ )
+    int err;
+
+    for (int i_note = state->logical_start[1]; i_note < state->logical_start[1] + state->semitones_in_grid; i_note ++ )
     {
-        line_index = state->grid_size[1] - VE_BOTT_LABELS_HEIGHT - ( i_note - state->logical_start[1] ) * LINES_PER_NOTE;
-
-        if (mvwprintw( tgt, line_index, VE_LEFT_LABELS_WIDTH, "%s", line ))
+        line_index = GRID_PADDING_TOP + ((state->grid_size[1] - GRID_BOTT_LABELS_HEIGHT)- LINES_PER_SEMITONE * ( i_note - state->logical_start[1] ));
+        assert(line_index > 0 && line_index < state->grid_size[1]);
+        // draw horizontal line
+        for (int j1 = GRID_LEFT_LABELS_WIDTH; j1 < state->grid_size[0] - 1; j1 += 2 )
         {
-            return 1;
-        }
-
-        line_index--;
-        
-        // make columns        
-        for (int j_beat = state->logical_start[0]; j_beat < COLS_PER_BEAT * ( state->logical_start[0] + state->bars_in_screen * 4); j_beat++ )
-        {
-            if (mvwprintw( tgt, line_index, 10, "%s", "ooo" ))
+            if ( (err = mvwaddch( tgt, line_index, j1, note_delim )) )
             {
                 return 1;
             }
         }
+
+        // offset for "x" markers -> time
+        line_index--;
+        
+        // // make columns        
+        // for (int j_beat = state->logical_start[0]; j_beat < COLS_PER_BEAT * ( state->logical_start[0] + state->beats_in_grid ); j_beat+= 4 )
+        // {
+        //     col_index = j_beat * COLS_PER_BEAT + GRID_LEFT_LABELS_WIDTH;
+            
+        //     if ( (err = mvwaddch( tgt, line_index, col_index, bar_delim )) )
+        //     {
+        //         return 1;
+        //     }
+        // }
     }
     return 0;
 }
@@ -119,63 +131,30 @@ int render_note_labels( WINDOW *tgt, UiState *state )
         oct,
         note_index;
 
-    for (int i_note = state->logical_start[1]; i_note < state->logical_start[1] + state->notes_in_screen; i_note ++ )
+    for (int i_note = state->logical_start[1]; i_note < state->logical_start[1] + state->semitones_in_grid; i_note ++ )
     {
-        line_index = state->grid_size[1] - VE_BOTT_LABELS_HEIGHT - ( i_note - state->logical_start[1] ) * LINES_PER_NOTE;
+        line_index = GRID_PADDING_TOP + ((state->grid_size[1] - GRID_BOTT_LABELS_HEIGHT)- LINES_PER_SEMITONE * ( i_note - state->logical_start[1] ));
+        assert(line_index > 0 && line_index < state->grid_size[1]);
+
         oct = i_note / 12;
         note_index = i_note % 12;  
 
-        if (mvwprintw( tgt, line_index, 2, "%s", ALL_NOTES[note_index] ))
+        int err;
+        if (( err = mvwprintw( tgt, line_index, 3, "%s", ALL_NOTES[note_index]) ))
         {
-            return 1;
+            return err;
         }
-        if (mvwprintw( tgt, line_index, 4, "%d", oct ))
+        if (( err = mvwprintw( tgt, line_index, 5, "%d", oct )))
         {
-            return 1;
+            return err;
         }
     }
 
     return 0;
 }
 
-
-// static int key;
-int handle_keys( UiState *ui_state)
+int render_bar_labels( WINDOW *tgt, UiState *state )
 {
-    int key = getch();
-
-    if (key == ERR)
-    {
-        return 0; // No key pressed
-    }
-
-    switch (key)
-    {
-        case KEY_UP:
-            // Handle up arrow key
-            // (*dummyval)++;
-            break;
-        case KEY_DOWN:
-            // (*dummyval)--;
-            // Handle down arrow key
-            break;
-        case KEY_LEFT:
-            // Handle left arrow key
-            break;
-        case KEY_RIGHT:
-            // Handle right arrow key
-            break;
-        case 'q':
-        case 'Q':
-            ui_state->is_running = false;
-            break;
-        case 'e':
-        case 'E':
-            ui_state->is_dirty = !ui_state->is_dirty;
-            break;
-        default:
-            break;
-    }
     return 0;
 }
 
@@ -201,27 +180,81 @@ int render_midi_editor( UiState *ui_state, MiniMidi_File *file, WINDOW * target,
     int size_x, size_y;
     getmaxyx(target, size_y, size_x);
 
+    // for debug
+    WIN_SIZE[0] = size_x;
+    WIN_SIZE[1] = size_y;
+
     // update size things
     UiState_update_sizes(ui_state, size_x, size_y);
-
-    // if (mvwprintw( target, 1, 1, "outer_size: [%i, %i]", outer_size[0], outer_size[1]))
-    // {
-    //     return 1;
-    // }
-
-    // if (mvwprintw( target, 2, 1, "inner_size: [%i, %i]", size_x, size_y ))
-    // {
-    //     return 1;
-    // }
-
-    render_note_labels( target, ui_state );
-    render_grid( target, ui_state );
+    if (render_note_labels( target, ui_state )) return 1;
+    if (render_grid( target, ui_state )) return 1;
 
     return 0;
 }
 
+// static int key;
+int handle_keys( UiState *ui_state)
+{
+    int key = getch();
+
+    if (key == ERR)
+    {
+        return 0; // No key pressed
+    }
+
+    switch (key)
+    {
+        case KEY_UP:
+            // Handle up arrow key
+            if (ui_state->logical_start[1] < MAX_NOTE_VAL ){
+                ui_state->logical_start[1]++;
+            }
+
+            break;
+        case KEY_DOWN:
+            if (ui_state->logical_start[1] > 0){
+                ui_state->logical_start[1]--;
+            }
+            
+            // Handle down arrow key
+            break;
+        case KEY_LEFT:
+            
+            break;
+        case KEY_RIGHT:
+            // Handle right arrow key
+            break;
+        case 'q':
+        case 'Q':
+            ui_state->is_running = false;
+            break;
+        case 'e':
+        case 'E':
+            ui_state->is_dirty = !ui_state->is_dirty;
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+void quit( WINDOW *ed, MiniMidi_File *f, bool is_error )
+{
+    delwin( ed );
+    endwin();
+    
+    MiniMidi_File_free( f );
+    if (is_error)
+    {
+        printf(RED "ERROR" RESET "Houston we have a problem...");
+    }
+}
 
 
+
+/***
+ *  MAIN!
+ */
 int main( int argc, char *argv[] )
 {
     // Catch Args
@@ -262,6 +295,7 @@ int main( int argc, char *argv[] )
     MiniMidi_File *midi_file = MiniMidi_File_read_from_file( argv[1] );
     UiState ui_state;
     UiState_init( &ui_state );
+    int ERRSTATUS = 0;
 
     if (midi_file == NULL) {
         printf(RED "ERROR" RESET " Failed to read MIDI file: %s\n", argv[1]);
@@ -286,13 +320,20 @@ int main( int argc, char *argv[] )
         0
     );
 
-    // UiState_update_sizes( &ui_state, outer_size[0], outer_size[1] - TOP_BAR_HEIGHT - BOTT_BAR_HEIGHT );
-
     while (ui_state.is_running)
     {
         clear();
-        render_info( &ui_state, midi_file, outer_size );
-        render_midi_editor( &ui_state, midi_file, midieditor_derwin, outer_size );
+        if( render_info( &ui_state, midi_file, outer_size ) )
+        {
+            ERRSTATUS = 1;
+            break;
+        }
+
+        if ( render_midi_editor( &ui_state, midi_file, midieditor_derwin, outer_size ) )
+        {
+            ERRSTATUS = 1;
+            break;
+        }
 
         box( midieditor_derwin, '|', '=' );
         wrefresh( stdscr );
@@ -300,11 +341,8 @@ int main( int argc, char *argv[] )
         handle_keys( &ui_state );
     }
 
-    delwin( midieditor_derwin );
-    endwin();
-    
-    // MiniMidi_File_print( midi_file );
-    MiniMidi_File_free( midi_file );
+    quit(midieditor_derwin, midi_file, ERRSTATUS ? true: false);
 
+    printf("\nFinished! Used a WINDOW of size {cols=%d, lines=%d}.\n", WIN_SIZE[0], WIN_SIZE[1]);
     return 0;
 }
